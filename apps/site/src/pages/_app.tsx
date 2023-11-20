f1a566bf24ea5645032af3c63ca69916497637f7
Joshua Strobl <me@joshuastrobl.com>
2023-11-21 00:49:58 +0200

    chore: update next-intl to latest major

--- apps/site/src/pages/_app.tsx
+++ apps/site/src/pages/_app.tsx
@@ -1,22 +1,25 @@
 import type { AppProps } from "next/app";
 import type { AbstractIntlMessages } from "next-intl";
-import { NextIntlProvider } from "next-intl";
+import { NextIntlClientProvider } from "next-intl";
 import { inter } from "../fonts";
+import { useRouter } from "next/router";
 
 type Props = AppProps & {
   pageProps: { messages: AbstractIntlMessages };
 };
 
 const App = ({ Component, pageProps }: Props) => {
+  const router = useRouter();
+
   return (
-    <NextIntlProvider messages={pageProps.messages}>
+    <NextIntlClientProvider locale={router.locale} messages={pageProps.messages} timeZone="UTC">
       <Component
         className={{
           ...inter,
           ...pageProps,
         }}
       />
-    </NextIntlProvider>
+    </NextIntlClientProvider>
   );
 };
 
